c33730f1d4b783db7ace46a80d48941422c3d3d1
aradgh <arad.ghorreshi@gmail.com>
2024-05-31 10:58:08 +0200

    Spring init

---
+++ src/test/java/com/example/projet_5_openclassrooms/Projet5OpenClassroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_5_openclassrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet5OpenClassroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
