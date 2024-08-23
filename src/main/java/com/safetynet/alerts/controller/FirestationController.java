92029728502595cce46f1bdce5d704d727a5429a
aradgh <arad.ghorreshi@gmail.com>
2024-08-23 10:35:46 +0200

    Implementation of FirestationController.java

---
+++ src/main/java/com/safetynet/alerts/controller/FirestationController.java
@@ -0,0 +1,41 @@
+package com.safetynet.alerts.controller;
+
+import com.safetynet.alerts.model.Firestation;
+import com.safetynet.alerts.service.FirestationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/firestation")
+public class FirestationController {
+    @Autowired
+    private FirestationService firestationService;
+
+    @PostMapping
+    public ResponseEntity<String> addFirestation(@RequestBody Firestation firestation) {
+        firestationService.addFirestation(firestation);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Firestation added successfully.");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateFirestation(@RequestBody Firestation firestation) {
+        boolean updated = firestationService.updateFirestation(firestation);
+        if (updated) {
+            return ResponseEntity.ok("Firestation updated successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Firestation not found.");
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteFirestation(@RequestParam String address) {
+        boolean deleted = firestationService.deleteFirestation(address);
+        if (deleted) {
+            return ResponseEntity.ok("Firestation deleted successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Firestation not found.");
+        }
+    }
+}
