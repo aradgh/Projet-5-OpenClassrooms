9d2315cc175050c85415302feb46a6f291b2414d
aradgh <arad.ghorreshi@gmail.com>
2024-08-23 10:27:06 +0200

    Implementation of PersonService.java

--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -2,42 +2,33 @@ package com.safetynet.alerts.service;
 
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+@Service
 public class PersonService {
+    @Autowired
     private PersonRepository personRepository;
 
-   public List<Person> findAll() {
-        return personRepository.findAll();
+    public void addPerson(Person person) {
+        personRepository.save(person);
     }
 
-    public Person findByFirstNameAndLastName(String firstName, String lastName) {
-        return personRepository.findByFirstNameAndLastName(firstName, lastName);
+    public boolean updatePerson(Person person) {
+        Person existingPerson = personRepository.findByFirstNameAndLastName(person.getFirstName(), person.getLastName());
+        if (existingPerson != null) {
+            personRepository.save(person);
+            return true;
+        }
+        return false;
     }
 
-    public Person findByZip(String zip) {
-        return personRepository.findByZip(zip);
+    public boolean deletePerson(String firstName, String lastName) {
+        Person existingPerson = personRepository.findByFirstNameAndLastName(firstName, lastName);
+        if (existingPerson != null) {
+            personRepository.delete(existingPerson);
+            return true;
+        }
+        return false;
     }
-
-    public Person findByAddress(String address) {
-        return personRepository.findByAddress(address);
-    }
-
-    public Person findByCity(String city) {
-        return personRepository.findByCity(city);
-    }
-
-    public Person findByPhone(String phone) {
-        return personRepository.findByPhone(phone);
-    }
-
-    public Person findByEmail(String email) {
-        return personRepository.findByEmail(email);
-    }
-
-    public Person save(Person person) {
-        return personRepository.save(person);
-    }
-
 }
