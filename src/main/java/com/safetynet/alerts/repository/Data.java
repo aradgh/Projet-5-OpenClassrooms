47e24e484f4eadc528c518a66272d17e45c018bc
aradgh <arad.ghorreshi@gmail.com>
2024-08-25 09:35:49 +0200

    Implementing Data initialization with DataLoader.java, Data.java and SafetyNetAlertsApp.java

---
+++ src/main/java/com/safetynet/alerts/repository/Data.java
@@ -0,0 +1,37 @@
+package com.safetynet.alerts.repository;
+
+import com.safetynet.alerts.model.Firestation;
+import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.model.Person;
+
+import java.util.List;
+
+public class Data {
+    private List<Person> persons;
+    private List<Firestation> firestations;
+    private List<MedicalRecord> medicalRecords;
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    public List<Firestation> getFirestations() {
+        return firestations;
+    }
+
+    public void setFirestations(List<Firestation> firestations) {
+        this.firestations = firestations;
+    }
+
+    public List<MedicalRecord> getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    public void setMedicalRecords(List<MedicalRecord> medicalRecords) {
+        this.medicalRecords = medicalRecords;
+    }
+}
---
+++ src/main/java/com/safetynet/alerts/repository/DataLoader.java
@@ -0,0 +1,19 @@
+package com.safetynet.alerts.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DataLoader {
+    private static final String DATA_FILE = "data.json";
+
+    public static void loadData(PersonRepository personRepo, FirestationRepository firestationRepo, MedicalRecordRepository medicalRecordRepo) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Data data = mapper.readValue(new File(DATA_FILE), Data.class);
+
+        personRepo = new PersonRepository(data.getPersons());
+        firestationRepo = new FirestationRepository(data.getFirestations());
+        medicalRecordRepo = new MedicalRecordRepository(data.getMedicalRecords());
+    }
+}
