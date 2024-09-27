c6cdca4f1db0040696801863a791a7e8af0a31d7
aradgh <arad.ghorreshi@gmail.com>
2024-09-27 21:31:30 +0200

    Cleaning Firestation and MedicalRecord models

--- src/main/java/com/safetynet/alerts/model/Firestation.java
+++ src/main/java/com/safetynet/alerts/model/Firestation.java
@@ -3,16 +3,12 @@ package com.safetynet.alerts.model;
 import java.util.UUID;
 
 public class Firestation {
-    private UUID id = UUID.randomUUID();
+    private final UUID id = UUID.randomUUID();
     private String address;
     private String station;
 
     public UUID getId() {
         return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public String getAddress() {
--- src/main/java/com/safetynet/alerts/model/MedicalRecord.java
+++ src/main/java/com/safetynet/alerts/model/MedicalRecord.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class MedicalRecord {
-    private UUID id = UUID.randomUUID();
-//    private UUID personId;
+    private final UUID id = UUID.randomUUID();
     private String firstName;
     private String lastName;
     private String birthdate;
@@ -15,18 +14,6 @@ public class MedicalRecord {
     public UUID getId() {
         return id;
     }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-//    public UUID getPersonId() {
-//        return personId;
-//    }
-//
-//    public void setPersonId(UUID personId) {
-//        this.personId = personId;
-//    }
 
     public String getFirstName() {
         return firstName;
