41e4a310d10b74b9ba1e6010ddb827fbe4c3cf5b
aradgh <arad.ghorreshi@gmail.com>
2024-11-15 18:12:27 +0100

    Init logs and tests

--- src/test/java/com/example/projet_5_openclassrooms/SafetyNetAlertsAppTests.java
+++ src/test/java/com/example/projet_5_openclassrooms/SafetyNetAlertsAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Projet5OpenClassroomsApplicationTests {
+class SafetyNetAlertsAppTests {
 
 	@Test
 	void contextLoads() {
